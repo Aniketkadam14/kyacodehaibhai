6eb4524c49b38879443c1a44cbf7fd4b7c85e803
Aniket Kadam <abkadam1998@gmail.com>
2024-08-16 19:22:52 +0530

    sadda hak ithe rakh

---
+++ python/long.py
@@ -0,0 +1,19 @@
+#Open (or create) a file named 'example.txt' in write mode
+with open('example.txt', 'w') as file:
+    # Write some text to the file
+    file.write('Hello, World!\n')
+    file.write('This is a new file created using Python.')
+
+print("File has been created and text has been written.")
+
+
+file = open("example.txt", "r")
+print(file.read())
+
+with open("sallu.txt", "w") as mulla:
+    mulla.write("bhai gadi driver chala raha tha\nisme meri kya galti")
+print("sallu bhai ki file ban chuki hai")
+
+import os
+os.remove('sallu.txt')
+
